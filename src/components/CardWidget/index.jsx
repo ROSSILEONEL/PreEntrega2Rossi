e59a5949326f264210cd1ce1914d6825988907d7
unknown <leonelrossi1@gmail.com>
2023-03-01 01:43:46 -0300

    card and bootstrap

---
+++ src/components/CardWidget/index.jsx
@@ -0,0 +1,10 @@
+import './CardWidget.css'
+function CardWidget (){
+    return(
+        <div className='Card-Widget'>
+            <span className="hardcode">11</span>
+            <i  className="bi bi-cart nav-icon"> </i>
+        </div>)
+}
+        
+export default CardWidget;
--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -1,17 +1,19 @@
 
 import './Navbar.css'
+import '../CardWidget'
+import CardWidget from '../CardWidget';
 function NavBar(){
     return(
         <div className='div-navBar'>
-            <span className='nav-logo'>Lazy-SB</span>
-        <ul className='ul-navBar'>
-            <li> <a className='nav-items' href="/Home"> Home</a></li>
-            <li> <a className='nav-items' href="/Productos"> Productos</a></li>
-            <li> <a className='nav-items' href="/Galeria"> Galeria</a></li>
-            <li> <a className='nav-items' href="/Readers"> Readers</a></li>
-            <li> <a className='nav-items' href="/Contacto"> Contacto</a></li>
-        </ul>
+          
+            <ul className='ul-navBar'>
+                <li> <a className='nav-items' href="/Home"> Home</a></li>
+                <li> <a className='nav-items' href="/Productos"> Productos</a></li>
+                <li> <a className='nav-items' href="/Galeria"> Galeria</a></li>
+                <li> <a className='nav-items' href="/Readers"> Readers</a></li>
+                <li> <a className='nav-items' href="/Contacto"> Contacto</a></li>
+            </ul>
+            <CardWidget />
         </div>
-    );
-}
+    );}
 export default NavBar;
